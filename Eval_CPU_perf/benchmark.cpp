a375954e8472bbd4be9ae6d56f0eb7b1c3ad11ed
PawelFats <pawelfats@gmail.com>
2024-10-24 23:34:41 +0700

    Update prog

--- Eval_CPU_perf/benchmark.cpp
+++ Eval_CPU_perf/benchmark.cpp
@@ -5,50 +5,53 @@
 #include <fstream>
 #include <cstdlib>
 #include <sys/time.h>
+#include <random>
 
 using namespace std;
 
-// Функции для выполнения различных задач
-double benchmark_task_1_double(int n) {
-    double result = 0;
-    for (int i = 0; i < n; ++i) {
-        result += sin(i) * cos(i);
-    }
-    return result;
+template<typename T>
+T factorial(T n) {
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n - 1);
 }
 
-float benchmark_task_1_float(int n) {
-    float result = 0;
-    for (int i = 0; i < n; ++i) {
-        result += sin(i) * cos(i);
+template<typename T>
+T sum_of_squares(T n) {
+    T sum = 0;
+    for (T i = 1; i <= n; ++i) {
+        sum += i * i;
     }
-    return result;
+    return sum;
 }
 
-int benchmark_task_1_int(int n) {
-    int result = 0;
-    for (int i = 0; i < n; ++i) {
-        result += i * (i + 1);
-    }
-    return result;
+template<typename T>
+T compute_log(T n) {
+    return log(n);
 }
 
-// Функция для измерения времени с помощью gettimeofday
-double get_time() {
-    struct timeval time;
+
+// Функция для измерения времени с помощью gettimeofday (с точностью до микросекунд)
+double get_time(){
+    // Структура представляющая календарное время
+    struct timeval time; 
     gettimeofday(&time, NULL);
     return time.tv_sec + (time.tv_usec / 1000000.0);
 }
 
 // Запуск бенчмарка
-template<typename T>
-void run_benchmark(int trials, int n, const string& task_name, T (*task)(int), const string& type, const string& opt_flag) {
-    vector<double> times;
+template<typename T> 
+void run_benchmark(int trials, const string& task_name, T (*task)(int), const string& type){
+    // Вектор для хранения времени выполнения каждого теста
+    vector<double> times; 
     double start, end;
 
-    for (int i = 0; i < trials; ++i) {
+    // Генерация случайных данных
+    srand(static_cast<unsigned int>(time(0)));
+    int n = rand() % 10000 + 1; // Генерация случайного числа от 1 до 10000
+    
+    for (int i = 0; i < trials; ++i){ 
         start = get_time();
-        task(n);
+        task(n); // запуск теста
         end = get_time();
         double time_taken = end - start;
         times.push_back(time_taken);
@@ -56,41 +59,72 @@ void run_benchmark(int trials, int n, const string& task_name, T (*task)(int), c
 
     // Вычисление среднего времени
     double sum = 0;
-    for (double t : times) {
-        sum += t;
+    for (double t : times){
+        // Сумма времени выполнения всех тестов
+        sum += t; 
     }
+    // Среднее время выполнения
+    double avg_time = sum / trials;  
 
-    double avg_time = sum / trials;
+    // Вычисление дисперсии для того, чтобы найти отклонение 
     double variance = 0;
-    for (double t : times) {
-        variance += (t - avg_time) * (t - avg_time);
+    for (double t : times){
+        // Сумма квадратов отклонения каждого времени от среднего
+        variance += (t - avg_time) * (t - avg_time); 
     }
-    variance /= trials;
-    double stddev = sqrt(variance);
+    // Дисперсия
+    variance /= trials; 
+    // Старндратное отклоенние 
+    double stddev = sqrt(variance); 
+
+    // Простая производительность как размер задачи, делённый на время
+    // int task_perf = n / avg_time; 
+    double task_perf = trials / sum;
 
     // Сохранение результатов в файл
     ofstream file;
     file.open("benchmark_results.csv", ios::app);
-    file << "ProcessorModel, " << task_name << ", " << type << ", " << opt_flag << ", " << n << ", GETTIMEOFDAY, " << trials
-         << ", " << avg_time << ", " << stddev << ", " << (stddev / avg_time) * 100 << "%\n";
+    file << "AMD Ryzen 7 5800H, " 
+        << task_name //Тип теста 
+        << ", " 
+        << type      //Тип данных 
+        << ", "
+        << "None, "  //Ключ оптимизации 
+        << "insCount, " //Оценка числа инструкций при выполнении типовой задачи
+        << "GETTIMEOFDAY, " //Функция обращения к таймеру
+        << "time, " //время выполнения отдельного испытания
+        << "LNum, " //номер испытания типовой задачи
+        << trials    //Кол-во испытаний ??
+        << ", " 
+        << avg_time  //Среднее время выполнение теста
+        << ", " 
+        << stddev    // Старндратное отклоенние
+        << ", " 
+        << (stddev / avg_time) * 100 //Насколько сильно варьируются времена выполнения задачи по сравнению с их средним значением
+        << "%, " 
+        << task_perf << "\n"; // Производительность
     file.close();
 }
 
+
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        cerr << "Usage: " << argv[0] << " <number_of_trials> <task_size> <optimization_flag>\n";
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <number_of_trials>\n";
         return 1;
     }
 
     int trials = stoi(argv[1]);
-    int n = stoi(argv[2]);
-    string opt_flag = argv[3]; // Оптимизационные флаги (например, O1, O2, O3)
+    if (trials < 10) {
+        trials = 10;
+        cout << "The total number of tests is at least 10. Number_of_trials changed to 10!";
+    }
 
-    // Запуск с разными типами данных
-    run_benchmark(trials, n, "Task1_sin_cos", benchmark_task_1_double, "DOUBLE", opt_flag);
-    run_benchmark(trials, n, "Task1_sin_cos", benchmark_task_1_float, "FLOAT", opt_flag);
-    run_benchmark(trials, n, "Task1_sin_cos", benchmark_task_1_int, "INT", opt_flag);
+    // Запуск с конкретным типом данных (например, int)
+    run_benchmark<int>(trials, "Factorial", factorial<int>, "INT");
+    run_benchmark<int>(trials, "Sum_of_squares", sum_of_squares<int>, "INT");
+    run_benchmark<int>(trials, "Compute_log", compute_log<int>, "INT");
 
     cout << "Benchmark completed. Results saved to benchmark_results.csv\n";
     return 0;
 }
+
